0a3b300cddd200e5e7de8fbcbbc2f47b343c690d
Yabesh J <jyabeshyabesh@gmail.com>
2026-09-21 13:15:37 +0530

    Initial containerized Flask application

---
+++ app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/health")
+def health():
+    return jsonify(
+        status="healthy",
+        service="aws-devops-platform"
+    ), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
